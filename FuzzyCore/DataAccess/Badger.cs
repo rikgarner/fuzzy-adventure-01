b55f761e893189825c463a426b53ae7d894eb56a
Rick Garner <rikgarner@hotmail.com>
2024-03-17 23:44:37 +0000

    Init commit

---
+++ FuzzyCore/DataAccess/Badger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzyCore.DataAccess
+{
+    internal class Badger
+    {
+        public async Task<string> GetInfo(int id)
+        {
+            return ""+id;
+        }
+    }
+}
